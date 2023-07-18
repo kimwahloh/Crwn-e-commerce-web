40ab07d2aa3ca754044b1bf1bc4518a0ff152ed2
kimwahloh <kimwah000@gmail.com>
2023-07-18 10:54:53 +0800

    created checkout page along with checkout items and checkout cart functionality

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -22,22 +22,56 @@ const addCartItem = (cartItems, productToAdd) => {
     return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
+//123 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+    //find the cart item to remove 
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+    );
+
+    //check if qty is equal to 1, if it is, remove that item from the cart
+    //如果两个id一样就被remove, filter掉
+    if(existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    //return back cartitems with matching cart item with reduced qty
+    return cartItems.map((cartItem) =>
+        cartItem.id === cartItemToRemove.id
+            ? { ...cartItem, quantity: cartItem.quantity - 1 }
+            : cartItem
+    );  
+};
+
+const clearCartItem = (cartItems, cartItemToClear) => cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
 export const CartContext =  createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
-    cartCount: 0
+    removeItemToCart: () => {},
+    clearItemFromCart: () => {},
+    cartCount: 0,
+    cartTotal: 0
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartCount, setCartCount] = useState (0);
+    const [cartTotal, setCartTotal] = useState (0);
 
     useEffect(() => {
         const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0)
         setCartCount(newCartCount);
+    }, [cartItems])
+
+
+    //126 Cart Total
+    useEffect(() => {
+        const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price , 0)
+        setCartTotal (newCartTotal);
     }, [cartItems])
 
     //118 this function triggered when a user clicks on the button "ADD TO CART"
@@ -45,7 +79,15 @@ export const CartProvider = ({ children }) => {
         setCartItems(addCartItem(cartItems,productToAdd));
     };
 
-    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount };
+    const removeItemToCart = (cartItemToRemove) => {
+        setCartItems(removeCartItem(cartItems,cartItemToRemove));
+    };
+
+    const clearItemFromCart = (cartItemToClear) => {
+        setCartItems(clearCartItem(cartItems,cartItemToClear));
+    };
+
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, removeItemToCart, clearItemFromCart, cartItems, cartCount, cartTotal };
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 
